8ec698954d8bca9f865130fd096ce7040843b8c4
Robert Whalen <RobertWhalenBusiness@hotmail.com>
2024-04-30 06:11:25 +0100

    Add toast container to page root

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,45 @@
-import Hero from "@/Components/Hero";
+import Footer from "@/Components/Footer";
+import LandingArea from "@/Components/LandingArea";
 import MobileNavBar from "@/Components/MobileNavBar";
 import NavBar from "@/Components/NavBar";
 import React, { useState } from "react";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const HomePage = () => {
   const [open, setOpen] = useState<boolean>(false);
 
-  return <div className='overflow-x-hidden'>
+  return <main className='overflow-x-hidden'>
+    <ToastContainer
+      position="top-center"
+      autoClose={3000}
+      hideProgressBar
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="dark"
+      bodyClassName="default-font-family"
+    />
     <div>
       {/* Nav Bar */}
       <NavBar openMenu={() => setOpen(true)} />
       <MobileNavBar open={open} closeMenu={() => setOpen(false)} />
-      {/* Hero */}
-      <Hero />
+      {/* Landing Area */}
+      <LandingArea />
+      {/* Content */}
+      <section className='relative z-[30]'>
+        {/* <About /> */}
+        {/* <Skills /> */}
+        {/* <Projects /> */}
+        {/* <Testimonials /> */}
+      </section>
+      {/* Footer */}
+      <Footer />
     </div>
-  </div>
+  </main>
 };
 
 export default HomePage;
