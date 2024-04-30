521095bb102402aa5b477a91d58536cebaa997cf
Robert Whalen <RobertWhalenBusiness@hotmail.com>
2024-04-30 03:28:47 +0100

    rearrange bootstrap structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,7 +28,9 @@ const HomePage = () => {
   //   });
   // }, []);
 
-  return <div className="text-9xl text-red-800">WEBDEV WARRIORS</div>;
+  return <div>
+    This is the beginning
+  </div>
 };
 
 export default HomePage;
