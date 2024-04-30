d5aca4d0de85df522783ab5929b8ac9031faf346
Robert Whalen <RobertWhalenBusiness@hotmail.com>
2024-04-30 06:59:20 +0100

    Skeletonised sections

--- Components/MobileNavBar.tsx
+++ Components/MobileNavBar.tsx
@@ -11,14 +11,12 @@ const MobileNavBar = ({ open, closeMenu }: Props) => {
 
     return (
         <div className={`${navAnimation} fixed transform transition-all duration-300 top-0 left-0 right-0 bottom-0 z-[1000] bg-[#09101a]`}>
-            <div className='w-[100vh] h-[100vh] flex flex-col items-center justify-center'>
-                <div className='nav-link-mobile'>ICON</div>
-                <div className='nav-link-mobile'>ICON</div>
-                <div className='nav-link-mobile'>ICON</div>
-                <div className='nav-link-mobile'>ICON</div>
-                <div className='nav-link-mobile'>ICON</div>
-                <div className='nav-link-mobile'>ICON</div>
-                <div className='nav-link-mobile'>ICON</div>
+            <div className='w-[80vh] h-[100vh] flex flex-col items-center justify-center'>
+                <div className='nav-link-mobile'>About</div>
+                <div className='nav-link-mobile'>Services</div>
+                <div className='nav-link-mobile'>Skills</div>
+                <div className='nav-link-mobile'>Projects</div>
+                <div className='nav-link-mobile'>Testimonials</div>
             </div>
             <div
                 className='absolute cursor-pointer top-4 right-4 w-4 h-4 text-yellow-400'
--- Components/NavBar.tsx
+++ Components/NavBar.tsx
@@ -10,17 +10,15 @@ const NavBar = ({ openMenu }: Props) => {
     return (
         <nav className='w-full top-0 h-[10vh] bg-[#131c27] shadow-md'>
             <div className='flex items-center justify-between h-full mx-2 sm:mx-24'>
-                <h1 className='cursor-pointer text-[30vw] text-white font-bold xs:text-5xl'>
+                <h1 className='cursor-pointer text-[300%] text-white font-bold xs:text-5xl'>
                     HOME
                 </h1>
-                <div className='flex justify-between w-[60%]'>
-                    <div className='nav-link'>ICON</div>
-                    <div className='nav-link'>ICON</div>
-                    <div className='nav-link'>ICON</div>
-                    <div className='nav-link'>ICON</div>
-                    <div className='nav-link'>ICON</div>
-                    <div className='nav-link'>ICON</div>
-                    <div className='nav-link'>ICON</div>
+                <div className='flex justify-between w-[50%]'>
+                    <div className='nav-link'>About</div>
+                    <div className='nav-link'>Services</div>
+                    <div className='nav-link'>Skills</div>
+                    <div className='nav-link'>Projects</div>
+                    <div className='nav-link'>Testimonials</div>
                     <div onClick={openMenu}>
                         <Bars3Icon className='absolute right-4 top-1 w-8 md:hidden h-16 cursor-pointer text-yellow-300' />
                     </div>
