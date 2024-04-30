ee6edbc468fdb7b74d79e64e35852c3dec752833
Robert Whalen <RobertWhalenBusiness@hotmail.com>
2024-04-30 04:49:57 +0100

    Add Hero to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Hero from "@/Components/Hero";
 import MobileNavBar from "@/Components/MobileNavBar";
 import NavBar from "@/Components/NavBar";
 import React, { useState } from "react";
@@ -7,8 +8,11 @@ const HomePage = () => {
 
   return <div className='overflow-x-hidden'>
     <div>
+      {/* Nav Bar */}
       <NavBar openMenu={() => setOpen(true)} />
       <MobileNavBar open={open} closeMenu={() => setOpen(false)} />
+      {/* Hero */}
+      <Hero />
     </div>
   </div>
 };
