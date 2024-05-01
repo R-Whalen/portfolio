43547ea27c11f94610b0be3353233afa3542b0cc
Robert Whalen <RobertWhalenBusiness@hotmail.com>
2024-05-01 09:24:58 +0100

    Add black background to typing carousel

--- Components/TextEffect.tsx
+++ Components/TextEffect.tsx
@@ -2,22 +2,24 @@ import { TypeAnimation } from 'react-type-animation';
 
 const TextEffect = () => {
     return (
-        <TypeAnimation
-            sequence={[
-                'Web Developer',
-                1000,
-                'Programmer',
-                1500,
-                'DevOps Engineer',
-                1500,
-                'Designer',
-                1500
-            ]}
-            wrapper="span"
-            speed={50}
-            className='text-[2rem] md:text-[3rem] text-[#55e6a5] font-bold uppercase'
-            repeat={Infinity}
-        />
+        <div className='relative z-30 bg-black'>
+            <TypeAnimation
+                sequence={[
+                    'Web Developer',
+                    1000,
+                    'Programmer',
+                    1500,
+                    'DevOps Engineer',
+                    1500,
+                    'Designer',
+                    1500
+                ]}
+                wrapper="span"
+                speed={50}
+                className='text-[2rem] md:text-[3rem] text-[#55e6a5] font-bold uppercase'
+                repeat={Infinity}
+            />
+        </div>
     );
 };
 
