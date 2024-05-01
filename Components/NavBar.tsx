a281d4886ef2e89a31c91012160c4dd907b24ced
Robert Whalen <RobertWhalenBusiness@hotmail.com>
2024-05-01 06:41:33 +0100

    Update responsive proportions

--- Components/NavBar.tsx
+++ Components/NavBar.tsx
@@ -8,12 +8,12 @@ interface Props {
 const NavBar = ({ openMenu }: Props) => {
 
     return (
-        <nav className='w-full top-0 h-[10vh] bg-[#131c27] shadow-md'>
-            <div className='flex items-center justify-between h-full mx-2 sm:mx-24'>
+        <nav className='w-full fixed top-0 h-[7vh] min-h-[80px] bg-[#131c27] shadow-md z-50'>
+            <div className='flex items-center justify-between h-full mx-2 sm:mx-20'>
                 <h1 className='cursor-pointer text-[300%] text-white font-bold xs:text-5xl'>
                     HOME
                 </h1>
-                <div className='flex justify-between w-[50%]'>
+                <div className='flex justify-between w-[70%] lg:w-[50%] xl:w-[40%]'>
                     <div className='nav-link'>About</div>
                     <div className='nav-link'>Services</div>
                     <div className='nav-link'>Skills</div>
