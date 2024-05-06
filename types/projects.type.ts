1e3ad15d380ce2772df96b1714f54d832d75301c
Robert Whalen <RobertWhalenBusiness@hotmail.com>
2024-05-06 05:22:20 +0100

    project interface and documentation

---
+++ types/projects.type.ts
@@ -0,0 +1,10 @@
+/**
+ * Dentoes a container of work with all associated required information for browsing
+ */
+export interface Project {
+    title: string,
+    description: string,
+    image: string,
+    link: string,
+    tags: string[]
+};
