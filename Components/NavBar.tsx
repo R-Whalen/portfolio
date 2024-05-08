4543d54ba21773ca54d9456d056f60389087afbd
Robert Whalen <RobertWhalenBusiness@hotmail.com>
2024-05-09 00:22:16 +0100

    fix hero reference

--- Components/NavBar.tsx
+++ Components/NavBar.tsx
@@ -18,7 +18,7 @@ const NavBar = ({ openMenu }: Props) => {
         <nav className='w-full fixed top-0 h-[7vh] min-h-[80px] bg-[#131c27] shadow-md z-50 opacity-95'>
             <div className='flex items-center justify-between h-full mx-8 sm:mx-20'>
                 <h1 className='cursor-pointer'>
-                    <Image alt='Whale Logo' src='/images/favicon.png' width={100} height={160} onClick={() => scroll('Landing')} />
+                    <Image alt='Whale Logo' src='/images/favicon.png' width={100} height={150} onClick={() => scroll('Landing')} />
                 </h1>
                 <div className='flex justify-between w-[70%] lg:w-[50%] xl:w-[40%]'>
                     <div className='nav-link' onClick={() => scroll('About')}>About</div>
