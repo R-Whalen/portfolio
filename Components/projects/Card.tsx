017d6377530a497ccb77d4ed96bd8a43736468a7
Robert Whalen <RobertWhalenBusiness@hotmail.com>
2024-05-09 01:19:47 +0100

    Update to project structure

--- Components/projects/Card.tsx
+++ Components/projects/Card.tsx
@@ -1,5 +1,6 @@
 import { Project } from '@/types'
-import React from 'react'
+import Image from 'next/image';
+import React, { Fragment } from 'react'
 
 const Card = ({
     title,
@@ -7,11 +8,31 @@ const Card = ({
     image,
     link,
     tags }: Project) => (
-    <a href={link} target='__blank'>
-        <div className='bg-red-700 hover:scale-110 transform transition-all duration-300 hover:-rotate-6 uppercase font-semibold text-center p-8'>
-            {image ? <div className={`h-16 w-16 bg-[url(${image})]`} /> : <div className='h-16 w-full'>&nbsp;</div>}
-            <h1 className='text=[20px] md:text-[30px] my-6'>{title}</h1>
-            <p className='text-[15px] text-[#d3fae8] font-normal'>{description}</p>
+    <a href={link} target='__blank' className='group hover:-translate-y-3 transform transition-all duration-300'>
+        <div>
+            <div className='w-full relative h-[300px] md:h-[400px]'>
+                <Image
+                    src={image}
+                    alt='Project'
+                    layout='fill'
+                    className='object-cover'
+                />
+            </div>
+            <div className='w-[90%] text-center mx-auto bg-[#09101a] relative mt-[-1rem] p-8 font-semibold transform transition-all duration-300 group-hover:shadow-xl group-hover:shadow-[#55e6a5]'>
+                <div className='w-fit px-4 my-4 bg-[#55e6a5] relative mt-[-3rem] text-black text-[17px] mx-auto'>
+                    {title}
+                </div>
+                <p className='pt-4 text-white text-[17px] font-normal'>{description}</p>
+                <div className='flex flex-row justify-center gap-3 flex-wrap pt-2'>
+                    {
+                        tags.map((tag: string) => <Fragment key={tag}>
+                            <div className='bg-yellow-400 px-2 rounded-sm'>
+                                {tag}
+                            </div>
+                        </Fragment>)
+                    }
+                </div>
+            </div>
         </div>
     </a>
 )
--- Components/projects/index.tsx
+++ Components/projects/index.tsx
@@ -4,19 +4,17 @@ import FeaturedProjects from '../../public/data/featured-projects.json';
 import { Project } from '@/types';
 import Card from './Card';
 
-const Projects = () => {
-    return (
-        <div id='Projects' className='bg-[#02050a] pt-64 md:pt-32 pb-4'>
-            <h1 className='heading'>
-                Featured <span className='text-yellow-400'>Projects</span>
-            </h1>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-[80%] mx-auto items-center gap-12 mt-16 text-white'>
-                {FeaturedProjects.map((project: Project) => <Fragment key={project.title}>
-                    <Card {...project} />
-                </Fragment>)}
-            </div>
+const Projects = () => (
+    <div id='Projects' className='bg-[#02050a] pt-64 md:pt-32 pb-4'>
+        <h1 className='heading'>
+            Featured <span className='text-yellow-400'>Projects</span>
+        </h1>
+        <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 w-[80%] mx-auto items-center gap-12 mt-16 text-white'>
+            {FeaturedProjects.map((project: Project) => <Fragment key={project.title}>
+                <Card {...project} />
+            </Fragment>)}
         </div>
-    )
-}
+    </div>
+)
 
 export default Projects;
